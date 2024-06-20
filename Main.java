6d35009bf0743959c1dafddc9bd87d764c3096e7
AaronY77 <87675088+AaronY77@users.noreply.github.com>
2024-06-20 12:41:54 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,15 @@
 package com.company;
-
+import java.util.*;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Puzzle p = new Puzzle(5);
-        System.out.println(p);
-
+        System.out.println("Enter the dimension of the grid you wish to play on.");
+        Scanner scanner= new Scanner(Syststem.in);
+        int dimension  = scanner.nextInt();
+        Puzzle p = new Puzzle(dimension);
         new PicturePuzzle(p);
-        //new NumberButtonView(p);
+    
     }
 }
 
